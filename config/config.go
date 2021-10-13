1199add7691e6056d89b2feaf5d3055e04811fba
Arturs Bricis <arturs.bricis@swisscom.com>
2021-10-13 22:27:50 +0300

    parse config from .env;

--- config/config.go
+++ config/config.go
@@ -1,5 +1,13 @@
 package config
 
+import (
+	"fmt"
+	"os"
+	"strconv"
+
+	"github.com/joho/godotenv"
+)
+
 type TransmissionServer struct {
 	Host     string
 	Port     uint16
@@ -9,7 +17,7 @@ type TransmissionServer struct {
 }
 
 type RestServer struct {
-	Port        int
+	Port        uint16
 	ApiBasePath string
 	MainEntity  string
 }
@@ -24,24 +32,62 @@ type Config struct {
 	Log                Log
 }
 
-// TODO: unmarshal config from JSON
 func Load() *Config {
+
+	envConfig, err := godotenv.Read(getConfigSource())
+
+	if err != nil {
+		panic(fmt.Errorf("failed to load .env file with: %+v", err))
+	}
+
 	config := &Config{
 		TransmissionServer: TransmissionServer{
-			Host:     "127.0.0.1",
-			Port:     9091,
-			Protocol: "https",
-			Username: "John",
-			Password: "Smith",
+			Host:     envConfig["TRANSMISSION_HOST"],
+			Port:     stringToUint16(envConfig["TRANSMISSION_PORT"]),
+			Protocol: envConfig["TRANSMISSION_PROTOCOL"],
+			Username: envConfig["TRANSMISSION_USERNAME"],
+			Password: envConfig["TRANSMISSION_PASSWORD"],
 		},
 		RestServer: RestServer{
-			Port:        5000,
-			ApiBasePath: "api",
-			MainEntity:  "torrents",
+			Port:        stringToUint16(envConfig["SERVER_PORT"]),
+			ApiBasePath: envConfig["SERVER_BASE_PATH"],
+			MainEntity:  envConfig["SERVER_MAIN_ENTITY"],
 		},
 		Log: Log{
-			Level: "INFO",
+			Level: envConfig["LOG_LEVEL"],
 		},
 	}
 	return config
+}
+
+func getConfigSource() string {
+	var configSource string
+	isProduction := getEnvBool("IS_PROD", false)
+	if isProduction {
+		configSource = "config/PROD.env"
+	} else {
+		configSource = "config/LOCAL.env"
+	}
+	return configSource
+}
+
+func stringToUint16(value string) uint16 {
+	uInt64, err := strconv.ParseUint(value, 10, 16)
+	if err != nil {
+		panic(fmt.Errorf("failed to convert string(%s) to uint16 with: %+v", value, err))
+	}
+	return uint16(uInt64)
+}
+
+func getEnvBool(value string, fallback bool) bool {
+	envValue := os.Getenv(value)
+	if len(envValue) == 0 {
+		return fallback
+	}
+	valueBool, err := strconv.ParseBool(envValue)
+	if err != nil {
+		fmt.Printf("failed to parse string(%s) to bool with: %+v", value, err)
+		return fallback
+	}
+	return valueBool
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module artubric/transmission-rest
 
 go 1.16
 
-require github.com/hekmon/transmissionrpc/v2 v2.0.1
+require (
+	github.com/hekmon/transmissionrpc/v2 v2.0.1
+	github.com/joho/godotenv v1.4.0 // indirect
+)
